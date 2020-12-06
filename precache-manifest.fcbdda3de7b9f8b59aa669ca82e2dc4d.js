b50b162e38b762d93ce2c3ddb0600e11a91dd66b
kshvetsova <katshvetsova@gmail.com>
2020-12-07 01:20:09 +0200

    Updates

---
+++ precache-manifest.fcbdda3de7b9f8b59aa669ca82e2dc4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4540722b90ce6acaae3d4673e12df5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec16b9796f15aaaa1c3d",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "78e64c39886a9d4c888f",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "ec16b9796f15aaaa1c3d",
+    "url": "./static/js/2.f708835b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.f708835b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e64c39886a9d4c888f",
+    "url": "./static/js/main.bb758dfd.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
